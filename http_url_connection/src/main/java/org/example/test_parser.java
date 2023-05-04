3c4efa60a42a04a21549647c241a24a373ec1aae
Daedalus_02 <88882210+fish11111@users.noreply.github.com>
2023-05-04 18:21:03 +0200

    Update test_parser.java

--- http_url_connection/src/main/java/org/example/test_parser.java
+++ http_url_connection/src/main/java/org/example/test_parser.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class test_parser
 {
     public static void main(String Args[]) throws FileNotFoundException {
-        File file = new File("C:\\Users\\kriptos\\OneDrive - Università degli Studi di Padova\\università\\anno-2\\Secondo_semestre\\elementi_di_ingegneria_del_software\\materiale\\esercizi\\java\\http_url_connection\\http_url_connection\\src\\main\\java\\org\\example\\test.txt");
+        File file = new File("http_url_connection\\http_url_connection\\src\\main\\java\\org\\example\\test.txt");
         Scanner console = new Scanner(file);
         String str = "";
         while (console.hasNextLine()){
