d66f64682f0516fa35c8d487139e588316388656
Daedalus_02 <88882210+fish11111@users.noreply.github.com>
2023-05-04 14:15:50 +0200

    Add files via upload

--- http_url_connection/src/main/java/org/example/Main.java
+++ http_url_connection/src/main/java/org/example/Main.java
@@ -12,50 +12,95 @@ import java.util.*;
 
 public class Main {
     public static void main(String Args[]) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("enter the topic you want to investigate: ");
+        String query = console.next();
+
+        int tagNumber = 0;
+        System.out.println("Enter the number of tag you want to search: ");
+        tagNumber = console.nextInt();
+        String[] tags = new String[tagNumber];
+        for(int i = 0; i < tagNumber; i++ ){
+            tags[i] = console.next();
+            System.out.println();
+        }
+
+        int contentNunber = 0;
+        System.out.println("Enter the number of contents you want to search: ");
+        contentNunber = console.nextInt();
+        String[] contents = new String[contentNunber];
+        for(int i = 0; i < contentNunber; i++){
+            contents[i] = console.next();
+            System.out.println();
+        }
+
+        System.out.println("Enter the max number of page to elaborate: ");
+        int maxPage = console.nextInt();
+        int pageCount = 0;
+
         try {
-            String url = (new urlSetter("https://content.guardianapis.com", "c9d442dd-66ec-43a8-aa3d-26047fa8780e", 2, "politics", new String[]{}, new String[]{})).getUrl();
-            String apiString = "";
-            httpGetter getter = new httpGetter(new URL(url));
-            apiString = getter.getHttpString();
-
-            jsonParser jsonparser = new jsonParser(apiString);
-            article[] articles = jsonparser.getArticles();
-
-            htmlDownloader downloader = new htmlDownloader();
-            String[] fileNames = new String[articles.length];
-
-            int counter = 0;
-            String pageContent = " "; //must have length different from 0
-            String pageText = " ";
-
-            Tokenizer tokenizer = new Tokenizer(pageContent,true);
+            Tokenizer tokenizer = new Tokenizer("", true);
             htmlParser htmlparser = new htmlParser();
 
-            for (article  article : articles) {
-                fileNames[counter] = downloader.download(article.getWebUrl().toString());
-                Scanner reader = new Scanner(new File(fileNames[counter]));
-                while(reader.hasNextLine()){
-                    pageContent += reader.nextLine();
-                }
-                pageText = htmlparser.parse(pageContent);
-                tokenizer.addDocument(pageText);
-                //System.out.println(tokenizer.getSize());
-                counter++;
-                pageContent = "";
-            }
+            String pageContent = " "; //must have length different from 0
+            String pageText = " ";
+            String url = "";
+            httpGetter getter;
+            String apiString ="";
+            jsonParser jsonparser;
+            article[] articles;
+            htmlDownloader downloader;
+            String[] fileNames;
+            Scanner reader;
+            int availablePage = 0;
+            while(pageCount < maxPage) {
+                url = (new urlSetter("https://content.guardianapis.com", "c9d442dd-66ec-43a8-aa3d-26047fa8780e", pageCount+1, query, new String[]{}, new String[]{})).getUrl();
 
+                getter = new httpGetter(new URL(url));
+                apiString = getter.getHttpString();
+
+                jsonparser = new jsonParser(apiString);
+                articles = jsonparser.getArticles();
+                if(jsonparser.getPages() < maxPage){
+                    maxPage = jsonparser.getPages();
+                    System.out.println("Limited to "+maxPage+" pages...");
+                }
+
+                downloader = new htmlDownloader();
+                fileNames = new String[articles.length];
+
+
+
+                for (article article : articles) {
+                    if(pageCount == maxPage){
+                        break;
+                    }
+                    System.out.println("Anayzing site number: "+(pageCount+1)+" with title: "+article.getWebTitle());
+                    fileNames[pageCount%10] = downloader.download(article.getWebUrl().toString());
+                    reader = new Scanner(new File(fileNames[pageCount%10]));
+                    while (reader.hasNextLine()) {
+                        pageContent += reader.nextLine();
+                    }
+                    pageText = htmlparser.parse(pageContent);
+                    tokenizer.addDocument(pageText);
+                    pageCount++;
+                    pageContent = "";
+                }
+
+
+            }
             //printing 50 more frequent tokens
             System.out.println("The 50 more frequent words in the analyzed articles are: ");
-            Set<Map.Entry<Integer, List<String>>> set =  tokenizer.getOrderedTokens(50);
+            Set<Map.Entry<Integer, List<String>>> set = tokenizer.getOrderedTokens(50);
 
             int wordCounter = 0;
             Iterator<Map.Entry<Integer, List<String>>> iter = set.iterator();
             Map.Entry<Integer, List<String>> pair = iter.next();
-            while(iter.hasNext()) {
+            while (iter.hasNext()) {
                 int index = pair.getValue().size();
-                while(index > 0){
+                while (index > 0) {
                     index--;
-                    System.out.println("          "+(wordCounter+1)+": "+pair.getValue().get(index)+" "+pair.getKey());
+                    System.out.println("          " + (wordCounter + 1) + ": " + pair.getValue().get(index) + " " + pair.getKey());
                     wordCounter++;
                 }
                 pair = iter.next();
--- http_url_connection/src/main/java/org/example/jsonParser.java
+++ http_url_connection/src/main/java/org/example/jsonParser.java
@@ -64,4 +64,79 @@ public class jsonParser
         return articles;
     }
 
+    public String getJsonString() {
+        return jsonString;
+    }
+
+    public void setJsonString(String jsonString) {
+        this.jsonString = jsonString;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(int startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+
+    public void setArticles(article[] articles) {
+        this.articles = articles;
+    }
+
+    public String getUserTier() {
+        return userTier;
+    }
+
+    public void setUserTier(String userTier) {
+        this.userTier = userTier;
+    }
 }
