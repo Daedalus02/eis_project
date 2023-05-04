a3e60ae821d0cff1d6cb989ee6ebfd2d5e309cf9
Daedalus_02 <88882210+fish11111@users.noreply.github.com>
2023-05-04 21:37:16 +0200

    Update urlSetter_test.java

--- http_url_connection/src/main/java/org/example/urlSetter_test.java
+++ http_url_connection/src/main/java/org/example/urlSetter_test.java
@@ -4,7 +4,7 @@ public class urlSetter_test
 {
     public static void main(String Args[]){
         try {
-            urlSetter setter = new urlSetter("https://content.guardianapis.com", "c9d442dd-66ec-43a8-aa3d-26047fa8780e", 1, "politics", new String[]{}, new String[]{});
+            urlSetter setter = new urlSetter("https://content.guardianapis.com", "your_api_key", 1, "politics", new String[]{}, new String[]{});
             System.out.println(setter.getUrl());
         }catch(IllegalArgumentException e){
             e.printStackTrace();
