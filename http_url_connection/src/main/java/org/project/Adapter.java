ec9a8a0f8f37d9c6dfe44c4fdf5c6e9ef72ce55f
Daedalus_02 <88882210+fish11111@users.noreply.github.com>
2023-05-12 13:14:24 +0200

    Add files via upload

--- http_url_connection/src/main/java/org/project/Adapter.java
+++ http_url_connection/src/main/java/org/project/Adapter.java
@@ -1,4 +1,22 @@
 package org.project;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * this class allow for any user of this project to elaborate the data in a different way to the one used by the internal tokenizer
+ */
 public class Adapter {
+    private Tokenizer tokenizer;
+
+    //setting the internal tokenizer
+    public Adapter(Tokenizer tokenizer1){
+        tokenizer = tokenizer1;
+    }
+
+    //this returns a set of entry of String lists(words with same frequencies) as values and Integer(frequencies) as keys
+    public Set<Map.Entry<Integer, List<String>>> getOrderedTokens(int MaxSize){
+        return tokenizer.getOrderedTokens(MaxSize);
+    }
 }
