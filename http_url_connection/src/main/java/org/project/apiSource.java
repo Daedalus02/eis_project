6f6786b59210c619880eb7cd6ba54d4ae4bfb244
fish11111 <mattia.scantamburlo02@gmail.com>
2023-05-25 14:24:31 +0200

    add explanation comments for the  article sources.

--- http_url_connection/src/main/java/org/project/apiSource.java
+++ http_url_connection/src/main/java/org/project/apiSource.java
@@ -8,6 +8,9 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is used to represent an Articles source where the articles are read from an api response.
+ */
 public class apiSource implements articleSource{
     private httpGetter httpgetter;
     private htmlParser htmlparser;
@@ -16,18 +19,38 @@ public class apiSource implements articleSource{
     private int maxArticle;
     public final int intialCount = 1;
     private final int pageSize = 100;
-
+    private final String baseUrl = "https://content.guardianapis.com";
     private List<Article> articles;
+
+
+    /**
+     * This constructor sets the url and the max number of article to return, initializer the "urlsetter" with first value
+     * and the articles.
+     *
+     * @param apiKey which needs to by a valid api key of the "The Guardian" api page.
+     * @param tags which are used to specify a set of articles related to the strings passed.
+     * @param query which are used to search a specific set oof words inside alle possible articles.
+     * @param maxArticle1 which is the max number of article to return (so the actual number could be less)
+     * @throws IOException
+     * @throws JSONException
+     * @throws BadLocationException
+     */
     public apiSource(String apiKey, String[] tags, String[] query, int maxArticle1) throws IOException, JSONException, BadLocationException {
         //setting the max number of articles to read (could be less)
         maxArticle = maxArticle1;
         //setting url basing on the fields required for the api request
-        urlsetter = new urlSetter("https://content.guardianapis.com", apiKey, intialCount, pageSize, query, tags);
+        urlsetter = new urlSetter(baseUrl, apiKey, intialCount, pageSize, query, tags);
         //setting articles variable
         articles = new ArrayList<Article>();
         readArticle();
     }
 
+    /**
+     * This method read the Articles and parse(from html) the content of the head and body fields of the json response.
+     * @throws BadLocationException
+     * @throws IOException
+     * @throws JSONException
+     */
     private void readArticle() throws BadLocationException, IOException, JSONException {
         int articleCount = 0;
         String url = "";
@@ -68,7 +91,7 @@ public class apiSource implements articleSource{
             }
         }
 
-        //TRIM
+        //TRIM --> reducing the size the number of actual articles read
         articles = new ArrayList<Article>(articles.subList(0, maxArticle));
     }
 
--- http_url_connection/src/main/java/org/project/articleSource.java
+++ http_url_connection/src/main/java/org/project/articleSource.java
@@ -2,6 +2,13 @@ package org.project;
 
 import java.util.List;
 
+/**
+ * This interface is used to represent a general Source of Articles in the system.
+ */
 public interface articleSource {
+    /**
+     * This method is used by whoever needs the articles contained in a general Article source.
+     * @return List<Article> which represent the response of the source when asked to return its Articles.
+     */
     List<Article> getArticles();
 }
